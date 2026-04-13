7dfa3e07a24fdfe978f2238f7a443e59d5f09f31
이환제 <hwanjei2003@kyonggi.ac.kr>
2026-04-13 20:33:05 +0900

    feat: jwt인증기능 적용, 문서 수정기능 추가 (#16)

--- backend/api/documents.py
+++ backend/api/documents.py
@@ -1,17 +1,76 @@
 from fastapi import APIRouter, Depends
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy.sql import func
 from sqlalchemy.orm import Session
-
+from datetime import datetime
+from core.security import jwtAuth
 from core.database import get_db
 from models.document import Document
+import uuid
+from api.apiResponse import ApiResponse
 
 router = APIRouter(prefix="/documents", tags=["documents"])
 
-@router.get("")
-def read_documents(db: Session = Depends(get_db)):
+class UpdateDocuments(BaseModel):
+    title: str | None = None
+    category: str | None = None
+    captureDate: datetime | None = None
+    summary: str | None = None
+
+class DocumentInfo(BaseModel):
+    id: uuid.UUID
+    title: str
+    category: str
+    status: str
+    file_type: str
+    tags: list[str]
+    capture_date: datetime | None = None
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class DetailedInfo(BaseModel):
+    id: uuid.UUID
+    title: str
+    category: str
+    summary: str
+    status: str
+    file_url: str
+    file_type: str
+    tags: list[str]
+    capture_date: datetime | None = None
+    deadline: datetime | None = None
+    created_at: datetime
+
+    model_config = ConfigDict(from_attributes=True)
+
+class GetDocuments(BaseModel):
+    items: list[DocumentInfo]
+    pagination: dict
+
+
+@router.get("", response_model=ApiResponse[GetDocuments])
+def read_documents(jwtToken: dict = Depends(jwtAuth), db: Session = Depends(get_db)):
     documents = db.query(Document).all()
+
+    data = GetDocuments(
+        items=documents,
+        pagination={
+            "page": 1,
+            "size": len(documents),
+            "total": len(documents),
+            "has_next": False
+        }
+    )
+
+    return ApiResponse(
+        success=True,
+        message="문서 목록 조회 성공",
+        data=data
+    )
     
-    # 공통 Response 맞춰서 반환
+    """ # 공통 Response 맞춰서 반환
     return {
         "success": True,
         "message": "문서 목록 조회 성공",
@@ -24,10 +83,10 @@ def read_documents(db: Session = Depends(get_db)):
                 "has_next": False
             }
         }
-    }
+    } """
 
-@router.get("/{document_id}")
-def get_document(document_id: str, db: Session = Depends(get_db)):
+@router.get("/{document_id}", response_model=ApiResponse[DetailedInfo])
+def get_document(document_id: str, jwtToken: dict = Depends(jwtAuth), db: Session = Depends(get_db)):
     document = db.query(Document).filter(Document.id == document_id).first()
 
     if not document:
@@ -40,10 +99,60 @@ def get_document(document_id: str, db: Session = Depends(get_db)):
             }
         )
 
-    return {
-        "success": True,
-        "message": "문서 상세 조회 성공",
-        "data": {
-            "document": document
-        }
-    }
+    return ApiResponse(
+        success=True,
+        message="문서 상세 조회 성공",
+        data=document
+    )
+
+@router.patch("/{document_id}", response_model=ApiResponse[dict])
+def update_document(update: UpdateDocuments, document_id: str, jwtToken: dict = Depends(jwtAuth), db: Session = Depends(get_db)):
+
+    document = db.query(Document).filter(Document.id == document_id).first()
+
+    if not document:
+        return JSONResponse(
+            status_code=404,
+            content={
+                "success": False,
+                "message": "문서를 찾을 수 없습니다.",
+                "error_code": "DOCUMENT_NOT_FOUND"
+            }
+        )
+    
+    updateDict = update.model_dump(exclude_unset=True)
+    
+    for key, value in updateDict.items():
+        setattr(document, key, value)
+
+    document.updated_at = func.now()
+
+    db.commit
+    db.refresh(document)
+
+    return ApiResponse(
+        success=True,
+        message="문서 수정 성공",
+        data={}
+    )
+
+@router.delete("/{document_id}", response_model=ApiResponse[dict])
+def delete_document(document_id: str, jwtToken: dict = Depends(jwtAuth), db: Session = Depends(get_db)):
+    
+    document = db.query(Document).filter(Document.id == document_id).first()
+
+    if not document:
+        return JSONResponse(
+            status_code=404,
+            content={
+                "success": False,
+                "message": "문서를 찾을 수 없습니다.",
+                "error_code": "DOCUMENT_NOT_FOUND"
+            }
+        )
+
+    return ApiResponse(
+        success=True,
+        message="문서 삭제 성공",
+        data={}
+    )
