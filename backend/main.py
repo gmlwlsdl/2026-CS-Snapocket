25e975a84a6a2b4efa18a49529e32d2b10b58441
hwanjei <hwanjei2003@gmail.com>
2026-04-15 20:52:56 +0900

    feat: 태그 기능 추가

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from api import auth, analysis, documents, upload
+from api import auth, analysis, documents, upload, tags
 from graph.schema import data, router
 from graph.context import getContext
 from graph.qlRouter import CustomGraphQLRouter
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="Snapocket API", version="0.1.0")
 
@@ -19,12 +20,15 @@ app.include_router(auth.router)
 app.include_router(router)
 
 app.include_router(documents.router)
-# app.include_router(tags.router)
+app.include_router(tags.router)
 app.include_router(upload.router)
 app.include_router(analysis.router)
 
 graphql_app = CustomGraphQLRouter(data, context_getter=getContext)
 app.include_router(graphql_app, prefix="/graphql", tags=["graphql"])
+
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
+
 @app.get("/")
 def root():
     return {
