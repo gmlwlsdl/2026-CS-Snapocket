deeb00e30cd5c302b4bc1611f620aa3af0ea936c
gmlwlsdl <heejin094@naver.com>
2026-04-24 11:19:49 +0900

    feat: 허용 도메인에 배포 url 추가

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ def startup():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://snapocket.site"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
